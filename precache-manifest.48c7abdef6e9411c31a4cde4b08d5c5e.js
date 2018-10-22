9c286fe50d7d7e2fdcc9cf06b261795bc394826d
pcourpron <pierrecourpron@gmail.com>
2018-10-21 22:55:21 -0700

    Updates

---
+++ precache-manifest.48c7abdef6e9411c31a4cde4b08d5c5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9b3d6423e2b7352aebd",
+    "url": "/ClickGame/static/js/runtime~main.f9b3d642.js"
+  },
+  {
+    "revision": "56080243675d2824a442",
+    "url": "/ClickGame/static/js/main.56080243.chunk.js"
+  },
+  {
+    "revision": "f2139cf21936e2a358ea",
+    "url": "/ClickGame/static/js/1.f2139cf2.chunk.js"
+  },
+  {
+    "revision": "56080243675d2824a442",
+    "url": "/ClickGame/static/css/main.33a258bd.chunk.css"
+  },
+  {
+    "revision": "afce89197b131c1d62ab4cba8073abaf",
+    "url": "/ClickGame/index.html"
+  }
+];
